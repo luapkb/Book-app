38498b6f178865dc46e856ce87d1961377cbca90
paul bennett <luapkb@gmail.com>
2019-11-12 10:03:24 -0800

    synk

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ requier('dotenv').config();
 const PORT = process.env.PORT || 3002;
 const app = express();
 app.use(cors());
+app.use(express.static('./public'));
 
 app.get('/', openSearch);
 app.get('/searches'searchForBooks);
+
+function openSeach(req, res) {
+  res.render('pages/index');
+}
+
+function searchForBooks(req,res)
 app.listen(PORT, () => console.log('listening'));
